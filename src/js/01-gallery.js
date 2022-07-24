90c0856326d83f506a3b10f1c870fde96a7a08f9
Stanislav Sporevoi <stanislav.sporevoi@gmail.com>
2022-07-24 20:23:03 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,46 +1,23 @@
 // Add imports above this line
-import SimpleLightbox from "simplelightbox";
+
+import SimpleLightbox  from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
+
 // Change code below this line
 console.log(galleryItems);
 
 const galleryRef = document.querySelector('.gallery');
-galleryRef.addEventListener('click', openOriginalImg);
 galleryItems.forEach(createGalleryItem);
 
 function createGalleryItem ({ preview, original, description }) {
   galleryRef.insertAdjacentHTML('beforeend',
-    `<div class="gallery__item">
-        <a class="gallery__link" href="${original}">
-          <img
-            class="gallery__image"
-            src="${preview}"
-            data-source="${original}"
-            alt="${description}"
-          />
-        </a>
-    </div>`); 
-}
+    `<a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`); 
 
-
-function openOriginalImg(event) {
-  event.preventDefault();
-  const selectedItemUrl = event.target.dataset.source;
-  
-  const instance = basicLightbox.create(`
-    <img src=${selectedItemUrl} width="800" height="600">`
-    , {
-      onShow: () => {
-        window.addEventListener('keydown', onEscBtnClick)
-      },
-      onClose: () => {
-        window.removeEventListener('keydown', onEscBtnClick)
-      }
-    })
-    instance.show()
-  
-  function onEscBtnClick(event) {
-    event.code === 'Escape' && instance.close()
-  }
 }
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
